6b7cb8fd119c585b946977c129d9bdab1abb2b0f
syNisaa <anisaaabcd@gmail.com>
2023-11-19 00:43:03 +0700

    update tampilan frontend

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,30 @@ Route::get('/register', function () {
     return view('admin.register');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/courses', function () {
+    return view('courses');
+});
+
+Route::get('/event', function () {
+    return view('event');
+});
+
+Route::get('/portofolio', function () {
+    return view('portofolio');
+});
+
+Route::get('/mentor', function () {
+    return view('profile');
+});
+
 
 // Route Admin 
 Route::get('admin/dashboard', function () {
